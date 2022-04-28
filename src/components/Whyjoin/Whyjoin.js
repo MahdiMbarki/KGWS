88b9209018e6fe3ae8a70175841a4d72697387a5
Mahdi Mbarki <backarMahdi@gmailcom>
2022-04-28 20:05:22 +0100

    whyjoinedit

--- src/components/Whyjoin/Whyjoin.js
+++ src/components/Whyjoin/Whyjoin.js
@@ -1,6 +1,6 @@
 import './Whyjoin.css';
 import { useState } from 'react'
-import { Container, Row, Col, Card, Tab, Tabs, Accordion } from 'react-bootstrap';
+import { Container, Row, Col, Card, Tab, Tabs, Nav } from 'react-bootstrap';
 function Whyjoin() {
     const img1 = require('../../images/slider1/1.jpg')
 
@@ -68,32 +68,45 @@ function Whyjoin() {
                 <Container>
                     <Row>
                         <Col>
-                            <Accordion defaultActiveKey="0">
-                                <Accordion.Item eventKey="0">
-                                    <Accordion.Header onClick={handleenergy}>Branche énergie </Accordion.Header>
-                                    <Accordion.Body>
-                                        Distributeur des principaux acteurs de l’énergie pour les particuliers et les professionnels. Kompar accompagne ses clients dans la réalisation d’un bilan de leurs consommations énergétique afin de leur proposer l’offre la plus adaptées a leurs besoins. Depuis le 01 janvier 2007 le marché de l’énergie est ouvert à la concurrence. Cela signifie la fin du monopole d’état des fournisseurs historiques EDF et GDF et l’apparition des fournisseurs alternatifs. Quel que soit le fournisseur d’énergie, la qualité est toujours la même parce que le réseau est commun et alimente tous les compteurs. N’oublions pas que l’énergie c’est 7% des dépenses des ménages en France, soit 2300€ en moyenne par ménage et par an.
-                                    </Accordion.Body>
-                                </Accordion.Item>
-                                <Accordion.Item onClick={handletelecom} eventKey="1">
-                                    <Accordion.Header>Branche télécom </Accordion.Header>
-                                    <Accordion.Body>
-                                        Partenaire distributeur Bouygues Telecom Entreprises, nous mettons à votre disposition nos experts en télécommunications qui vous accompagneront pour faire de votre téléphonie d’entreprises un véritable avantage concurrentiel. Bouygues Telecom est un opérateur de télécommunications français, filiale du groupe Bouygues créé en 1994. Il est historiquement le troisième des quatre opérateurs de téléphonie mobile nationaux français, apparu après Orange et SFR et avant Free mobile. Avec des offres destinées aux professionnels comme aux entreprises de plus grande taille. Vous paierez vos factures téléphoniques au juste prix. N’attendez plus et demandez votre étude personnalisée et gratuite.
-                                    </Accordion.Body>
-                                </Accordion.Item>
-                                <Accordion.Item onClick={handlehealthcare} eventKey="2">
-                                    <Accordion.Header>Branche Assurance& Sante </Accordion.Header>
-                                    <Accordion.Body>
-                                        KOMPAR Assurances, enregistrée à L’Orias, est la division Assurances du Groupe KOMPAR. Nos agents conseillent et accompagnent les particuliers et les professionnels dans le choix de leurs complémentaires santés. Partenaire des plus grandes compagnies d’assurances et mutuelles, nous saurons vous proposer la solution la plus adaptée à vos besoins et à ceux de vos salariés.
-                                    </Accordion.Body>
-                                </Accordion.Item>
-                                <Accordion.Item onClick={handlesolution} eventKey="3">
-                                    <Accordion.Header>Solutions OUTSOURCING & Force de vente supplétive </Accordion.Header>
-                                    <Accordion.Body>
-                                        De part notre expérience dans plusieurs secteurs d’activités, nous avons développé une offre d’externalisation de force de vente dédiée aux entreprises qui souhaitent accroitre leurs parts de marché et leur chiffres d’affaires rapidement. Nous disposons de plusieurs centres de contacts avec un grand nombre de télévendeurs. Que vous souhaitiez adresser un marché de masse ou faire des opérations ciblées, que ce soit auprès des particuliers ou des professionnels, nous saurons mettre en place un partenariat « gagnant-gagnant ». Nos équipes se tiennent â votre disposition alors n’hésitez pas à faire appel à nous.
-                                    </Accordion.Body>
-                                </Accordion.Item>
-                            </Accordion>
+                            <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+                                <Row>
+                                    <Col sm={3}>
+                                        <Nav variant="pills" className="flex-column">
+                                            <Nav.Item>
+                                                <Nav.Link onClick={handleenergy} eventKey="first">Branche énergie</Nav.Link>
+                                            </Nav.Item>
+                                            <Nav.Item>
+                                                <Nav.Link onClick={handletelecom} eventKey="second">Branche télécom</Nav.Link>
+                                            </Nav.Item>
+                                            <Nav.Item>
+                                                <Nav.Link onClick={handlehealthcare} eventKey="third">Branche Assurance& Sante</Nav.Link>
+                                            </Nav.Item>
+                                            <Nav.Item>
+                                                <Nav.Link onClick={handlesolution} eventKey="fourth">Solutions OUTSOURCING & Force de vente supplétive </Nav.Link>
+                                            </Nav.Item>
+                                        </Nav>
+                                    </Col>
+                                    <Col sm={9}>
+                                        <Tab.Content>
+                                            <Tab.Pane eventKey="first">
+                                                Distributeur des principaux acteurs de l’énergie pour les particuliers et les professionnels. Kompar accompagne ses clients dans la réalisation d’un bilan de leurs consommations énergétique afin de leur proposer l’offre la plus adaptées a leurs besoins. Depuis le 01 janvier 2007 le marché de l’énergie est ouvert à la concurrence. Cela signifie la fin du monopole d’état des fournisseurs historiques EDF et GDF et l’apparition des fournisseurs alternatifs. Quel que soit le fournisseur d’énergie, la qualité est toujours la même parce que le réseau est commun et alimente tous les compteurs. N’oublions pas que l’énergie c’est 7% des dépenses des ménages en France, soit 2300€ en moyenne par ménage et par an.
+                                            </Tab.Pane>
+                                            <Tab.Pane eventKey="second">
+                                                Partenaire distributeur Bouygues Telecom Entreprises, nous mettons à votre disposition nos experts en télécommunications qui vous accompagneront pour faire de votre téléphonie d’entreprises un véritable avantage concurrentiel. Bouygues Telecom est un opérateur de télécommunications français, filiale du groupe Bouygues créé en 1994. Il est historiquement le troisième des quatre opérateurs de téléphonie mobile nationaux français, apparu après Orange et SFR et avant Free mobile. Avec des offres destinées aux professionnels comme aux entreprises de plus grande taille. Vous paierez vos factures téléphoniques au juste prix. N’attendez plus et demandez votre étude personnalisée et gratuite.
+
+                                            </Tab.Pane>
+                                            <Tab.Pane eventKey="third">
+                                                KOMPAR Assurances, enregistrée à L’Orias, est la division Assurances du Groupe KOMPAR. Nos agents conseillent et accompagnent les particuliers et les professionnels dans le choix de leurs complémentaires santés. Partenaire des plus grandes compagnies d’assurances et mutuelles, nous saurons vous proposer la solution la plus adaptée à vos besoins et à ceux de vos salariés.
+
+                                            </Tab.Pane>
+                                            <Tab.Pane eventKey="fourth">
+                                                De part notre expérience dans plusieurs secteurs d’activités, nous avons développé une offre d’externalisation de force de vente dédiée aux entreprises qui souhaitent accroitre leurs parts de marché et leur chiffres d’affaires rapidement. Nous disposons de plusieurs centres de contacts avec un grand nombre de télévendeurs. Que vous souhaitiez adresser un marché de masse ou faire des opérations ciblées, que ce soit auprès des particuliers ou des professionnels, nous saurons mettre en place un partenariat « gagnant-gagnant ». Nos équipes se tiennent â votre disposition alors n’hésitez pas à faire appel à nous.
+
+                                            </Tab.Pane>
+                                        </Tab.Content>
+                                    </Col>
+                                </Row>
+                            </Tab.Container>
 
                         </Col>
                         <Col>
